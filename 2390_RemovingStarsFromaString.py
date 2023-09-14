e3e5cc7c7d8a26309a277b44d850b8dd47d0169d
adbokat <oaki24138@gmail.com>
2023-09-14 19:44:59 +0300

    complete task 2390

---
+++ 2390_RemovingStarsFromaString.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+
+        for el in s:
+            if el == '*':
+                stack.pop()
+            else:
+                stack.append(el)
+
+        return ''.join(stack)
+
